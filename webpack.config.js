5709af43df1041a1f9153ca64a2b6dc281f1391b
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-07 12:00:13 +0530

    sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
             },
             {
                 test: /\.js$/,
